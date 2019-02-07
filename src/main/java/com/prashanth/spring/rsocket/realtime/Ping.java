b6b9fe9d71835722a0107d77ce4e151db4b80a8f
prashanth-g <prashanthcsgce@gmail.com>
2019-02-07 22:12:09 +0530

    Updated Ping client

--- src/main/java/com/prashanth/spring/rsocket/realtime/Ping.java
+++ src/main/java/com/prashanth/spring/rsocket/realtime/Ping.java
@@ -1,18 +1,41 @@
 package com.prashanth.spring.rsocket.realtime;
 
+import io.rsocket.Payload;
+import io.rsocket.RSocketFactory;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.util.DefaultPayload;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
+@Log4j2
+@Component
 public class Ping implements Ordered, ApplicationListener<ApplicationReadyEvent> {
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-
+        log.info("Starting "+this.getClass().getName());
+        RSocketFactory
+                .connect()
+                .transport(TcpClientTransport.create(8000))
+                .start()
+                .flatMapMany(rSocket ->
+                        rSocket.requestChannel (
+                            Flux.interval(Duration.ofSeconds(1)).map(i -> DefaultPayload.create("ping")))
+                        .map(Payload::getDataUtf8)
+                        .doOnNext(str -> log.info("received " + str + "in" + getClass().getName()))
+                        .take(10)
+                        .doFinally(signal -> rSocket.dispose())
+                ).then().block();
     }
 
     @Override
     public int getOrder() {
-        return 0;
+        return Ordered.LOWEST_PRECEDENCE;
     }
 }
