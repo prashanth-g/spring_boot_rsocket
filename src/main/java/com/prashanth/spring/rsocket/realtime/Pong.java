9702012e42771e24b2102e2f30af5008200c9b2d
prashanth-g <prashanthcsgce@gmail.com>
2019-02-08 18:20:12 +0530

    Updated Pong Server

--- src/main/java/com/prashanth/spring/rsocket/realtime/Pong.java
+++ src/main/java/com/prashanth/spring/rsocket/realtime/Pong.java
@@ -1,13 +1,14 @@
 package com.prashanth.spring.rsocket.realtime;
 
-import io.rsocket.ConnectionSetupPayload;
-import io.rsocket.RSocket;
-import io.rsocket.SocketAcceptor;
+import io.rsocket.*;
+import io.rsocket.transport.netty.server.TcpServerTransport;
 import lombok.extern.log4j.Log4j2;
+import org.reactivestreams.Publisher;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Log4j2
@@ -16,16 +17,31 @@ public class Pong implements SocketAcceptor, Ordered, ApplicationListener<Applic
 
     @Override
     public Mono<RSocket> accept(ConnectionSetupPayload setup, RSocket sendingSocket) {
+
+        AbstractRSocket abstractRSocket = new AbstractRSocket() {
+            @Override
+            public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
+                return Flux.from(payloads)
+                        .map(Payload::getDataUtf8)
+                        .doOnNext(str -> log.info("received "+str+ "in "+getClass().getName())).map(null);
+
+            }
+        };
         return null;
     }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-
+        RSocketFactory
+                .receive()
+                .acceptor(this)
+                .transport(TcpServerTransport.create(8000))
+                .start()
+                .subscribe();
     }
 
     @Override
     public int getOrder() {
-        return 0;
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 }
