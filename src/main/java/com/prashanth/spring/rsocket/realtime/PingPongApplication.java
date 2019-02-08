12b6562724604d25081c539f19a2477513d807e1
prashanth-g <prashanthcsgce@gmail.com>
2019-02-08 19:55:56 +0530

    Added reply method in PingPongApplication

--- src/main/java/com/prashanth/spring/rsocket/realtime/PingPongApplication.java
+++ src/main/java/com/prashanth/spring/rsocket/realtime/PingPongApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PingPongApplication {
+
+    static String reply(String in) {
+        if(in.equalsIgnoreCase("ping")) return "pong";
+        if(in.equalsIgnoreCase("pong")) return "ping";
+        throw new IllegalArgumentException("Incoming value must be either ping or pong");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PingPongApplication.class, args);
     }
